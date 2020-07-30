8a466344898ba3a46977ad63a7fa99dfec9f3fa7
Bob Verity <r.verity@imperial.ac.uk>
2020-07-30 06:36:27 +0100

    incremented version number for proper version tracking

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -430,8 +430,6 @@ double Particle::get_loglike(vector<double> &theta, int theta_i, bool quick_exit
       Rcpp::stop("individual-level probability outside [0,1] range");
     }
     
-    double z1, z2, z3, z4, z5, z6, z7, z8, z9;
-    
     // if no recorded ICU stay
     if (s_ptr->icu[i] == 0) {
       
